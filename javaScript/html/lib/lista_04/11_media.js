5443f3baad2648683272a0c39b24a306cf351845
commit <eduardoiury17@gmail.com>
2024-08-30 13:41:13 -0300

    mudanças da 11 da lista 04

--- javaScript/html/lib/lista_04/11_media.js
+++ javaScript/html/lib/lista_04/11_media.js
@@ -1,4 +1,18 @@
-function med(a){
 
-    return total
-}
+
+
+
+const { media } = require("./modulo");
+
+
+  console.log(media(1, 2, 3, 4, 5));
+  console.log(media(10, 20, 30, 40, 50)); 
+  console.log(media(5, 5, 5, 5, 5)); 
+  console.log(media([])); 
+  
+
+  // function media(a, b, c, d, e) { 
+//     let total = (a + b + c + d + e) / 5; 
+
+//     return total; 
+//   } 
